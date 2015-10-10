53d67c9c950d2c112645e7fb79f2694fbf84f47e
Stefano Chiesa <base278@gmail.com>
2015-10-11 03:11:47 +0800

    Refactor FileSender and FileReceiver

--- FileReceiver.java
+++ FileReceiver.java
@@ -7,14 +7,73 @@ import java.io.*;
 public class FileReceiver {
 
   private static final int POSITION_PORT_NUMBER = 0;
-  private static final int BLOCK_SIZE    = 576;
-  private static final int STRING_SIZE   = 256;
-  private static final int HEADER_SIZE   = 8;
-  private static final int CHECKSUM_SIZE = 8;
-  private static int CONTENT_WITH_NAME_SIZE  = BLOCK_SIZE - CHECKSUM_SIZE - HEADER_SIZE - STRING_SIZE;
-  private static int CONTENT_SIZE = BLOCK_SIZE - CHECKSUM_SIZE - HEADER_SIZE;
+
+  private final int BLOCK_SIZE              = 576;
+  private final int STRING_SIZE             = 256;
+  private final int SEQUENCE_SIZE           = 8;
+  private final int CHECKSUM_SIZE           = 8;
+  private final int HEADER_SIZE             = SEQUENCE_SIZE + CHECKSUM_SIZE;
+  private final int CONTENT_SIZE            = BLOCK_SIZE    - HEADER_SIZE;
+
+  DatagramSocket socket;
+  CRC32 crc;
+
+  public FileReceiver(Integer portNumber) throws Exception {
+    crc = new CRC32();
+    socket = new DatagramSocket(portNumber);
+  }
+
+  private int receivePacket(byte[] data) throws Exception {
+    DatagramPacket packet = new DatagramPacket(data, BLOCK_SIZE);
+    socket.receive(packet);
+    return packet.getLength();
+  }
+
+  private void receiveFile() throws Exception {
+    OutputStream destinationStream = null;
+
+    byte[] data = new byte[BLOCK_SIZE];
+    byte[] container = new byte[BLOCK_SIZE];
+    ByteBuffer dataBuffer = ByteBuffer.wrap(data);
+
+    while(true) {
+      dataBuffer.clear();
+      int packetLength = receivePacket(data);
+
+      if (packetLength < CHECKSUM_SIZE) {
+        throw new Exception("Packet too short");
+      }
+
+      long checksum = dataBuffer.getLong();
+
+      // compare checksum
+      crc.reset();
+      crc.update(data, CHECKSUM_SIZE, packetLength - CHECKSUM_SIZE);
+
+      if (checksum != crc.getValue()) {
+        throw new Exception("Packet corrupt");
+      }
+      else {
+        long sequenceNumber = dataBuffer.getLong();
+
+        if (sequenceNumber == 0) {
+          dataBuffer.get(container, 0, STRING_SIZE);
+
+          // Create an output stream
+          String destinationFile = new String(container).trim();
+          destinationStream = new BufferedOutputStream(new FileOutputStream(destinationFile));
+        }
+        else {
+          dataBuffer.get(container, 0, packetLength - HEADER_SIZE);
+
+          destinationStream.write(container, 0, packetLength - HEADER_SIZE);
+        }
+      }
+    }
+  }
 
   public static void main(String args[]) throws Exception {
+
     if (args.length != 1) {
       System.err.println("Usage: FileReceiver <port>");
       System.exit(-1);
@@ -22,82 +81,8 @@ public class FileReceiver {
 
     Integer portNumber = Integer.parseInt(args[POSITION_PORT_NUMBER]);
 
-    System.out.println(portNumber);
+    FileReceiver fileReceiver = new FileReceiver(portNumber);
 
-    
-    CRC32 crc = new CRC32();
-    DatagramSocket sk = new DatagramSocket(portNumber);
-
-    byte[] data = new byte[BLOCK_SIZE];
-    ByteBuffer dataBuffer = ByteBuffer.wrap(data);
-    OutputStream destinationFileStream = null;
-
-
-    int co = 0;
-    while(true) {
-      dataBuffer.clear();
-      DatagramPacket pkt = new DatagramPacket(data, BLOCK_SIZE);
-
-      co++;
-      //System.out.println("makasi " + co);
-
-      sk.receive(pkt);
-      int pktLen = pkt.getLength();
-      System.out.println(pkt.getLength());
-      
-      
-      if (pkt.getLength() < CHECKSUM_SIZE) {
-        System.out.println("Pkt too short");
-        assert(false);
-        continue;
-      }
-      
-      long chksum = dataBuffer.getLong();
-
-      crc.reset();
-      crc.update(data, 8, pktLen-8);
-
-      if (crc.getValue() != chksum)
-      {
-        System.out.println("Pkt corrupt");
-      }
-      else
-      {
-        long sequenceNumber = dataBuffer.getLong()+1;
-        //System.out.println("Pkt " + sequenceNumber);
-        assert(sequenceNumber == co);
-        
-        
-        
-        if (sequenceNumber == 1) {
-          byte[] name = new byte[STRING_SIZE];
-          byte[] lol = new byte[CONTENT_WITH_NAME_SIZE];
-          dataBuffer.get(name, 0, STRING_SIZE);
-          int remain = pktLen-16-STRING_SIZE;
-          dataBuffer.get(lol);
-          String destinationFileLocation = new String(name).trim();
-          System.out.println(destinationFileLocation);
-          destinationFileStream = new BufferedOutputStream(new FileOutputStream(destinationFileLocation));
-          destinationFileStream.write(lol, 0, remain);
-          System.out.println("success");
-        }
-        else {
-          byte[] lol = new byte[CONTENT_SIZE];
-          int remain = pktLen-16;
-          dataBuffer.get(lol, 0, CONTENT_SIZE);
-          destinationFileStream.write(lol, 0, remain);
-          //System.out.println(new String(lol));
-          System.out.println(remain);
-          System.out.println(CONTENT_SIZE);
-          if (remain != CONTENT_SIZE) {
-            destinationFileStream.close();
-            break;
-          }
-        }
-      }
-      dataBuffer.rewind();
-    }
-
-    //destinationFileStream.close();   
+    fileReceiver.receiveFile();
   }
 }
--- FileSender.java
+++ FileSender.java
@@ -6,96 +6,94 @@ import java.io.*;
 
 public class FileSender {
 
-  private static int POSITION_HOST_NAME = 0;
-  private static int POSITION_PORT_NUMBER = 1;
-  private static int POSITION_SOURCE_FILE_LOCATION = 2;
-  private static int POSITION_DESTINATION_FILE_LOCATION = 3;
+  private static final int POSITION_HOST_NAME = 0;
+  private static final int POSITION_PORT_NUMBER = 1;
+  private static final int POSITION_SOURCE_FILE_LOCATION = 2;
+  private static final int POSITION_DESTINATION_FILE_LOCATION = 3;
 
-  private static int BLOCK_SIZE    = 576;
-  private static int STRING_SIZE   = 256;
-  private static int HEADER_SIZE   = 8;
-  private static int CHECKSUM_SIZE = 8;
-  private static int CONTENT_WITH_NAME_SIZE  = BLOCK_SIZE - CHECKSUM_SIZE - HEADER_SIZE - STRING_SIZE;
-  private static int CONTENT_SIZE = BLOCK_SIZE - CHECKSUM_SIZE - HEADER_SIZE;
+  private final int BLOCK_SIZE              = 576;
+  private final int STRING_SIZE             = 256;
+  private final int SEQUENCE_SIZE           = 8;
+  private final int CHECKSUM_SIZE           = 8;
+  private final int HEADER_SIZE             = SEQUENCE_SIZE + CHECKSUM_SIZE;
+  private final int CONTENT_SIZE            = BLOCK_SIZE    - HEADER_SIZE;
+
+  InetSocketAddress receiverAddress;
+  CRC32 crc;
+  DatagramSocket socket;
+
+  public FileSender(String hostName, Integer portNumber) throws Exception {
+    receiverAddress = new InetSocketAddress(hostName, portNumber);
+    crc = new CRC32();
+    socket = new DatagramSocket();
+  }
+
+  public String getNormalizedString(String fileName) {
+    StringBuffer sb = new StringBuffer(fileName);
+    while(sb.length() < STRING_SIZE) {
+      sb.append(' ');
+    }
+    return sb.toString();
+  }
+
+  public void sendPacket(long sequenceNumber, byte[] data, int length) throws Exception {
+    ByteBuffer dataBuffer = ByteBuffer.allocate(HEADER_SIZE + length);
+
+    // reserve for checksum
+    dataBuffer.putLong(0);
+
+    // add sequence number
+    dataBuffer.putLong(sequenceNumber);
+
+    // add content
+    dataBuffer.put(data, 0, length);
+
+    crc.reset();
+    crc.update(dataBuffer.array(), CHECKSUM_SIZE, HEADER_SIZE + length - CHECKSUM_SIZE);
+
+    // add checksum
+    dataBuffer.rewind();
+    dataBuffer.putLong(crc.getValue());
+
+    // send packet
+    DatagramPacket packet = new DatagramPacket(dataBuffer.array(), HEADER_SIZE + length, receiverAddress);
+    socket.send(packet);
+
+  }
+
+  private void sendFile(String sourceFile, String destinationFile) throws Exception {
+
+    InputStream sourceStream = new BufferedInputStream(new FileInputStream(sourceFile));
+
+    byte[] data = new byte[BLOCK_SIZE];
+    ByteBuffer b = ByteBuffer.wrap(data);
+
+    long sequenceNumber = 0;
+
+    sendPacket(sequenceNumber++, getNormalizedString(destinationFile).getBytes(), 256);
+
+    // send packet
+    int length;
+    while((length = sourceStream.read(data, 0, CONTENT_SIZE)) > 0) {
+      sendPacket(sequenceNumber++, data, length);
+    }
+  }
 
   public static void main(String args[]) throws Exception {
-     
+
     if (args.length != 4) {
       System.err.println("Usage: FileSender <host> <port> <source_file> <destination_file>");
       System.exit(-1);
     }
 
-    String hostName = args[POSITION_HOST_NAME];
+    String hostName    = args[POSITION_HOST_NAME];
     Integer portNumber = Integer.parseInt(args[POSITION_PORT_NUMBER]);
-    String sourceFileLocation = args[POSITION_SOURCE_FILE_LOCATION];
+
+    String sourceFileLocation      = args[POSITION_SOURCE_FILE_LOCATION];
     String destinationFileLocation = args[POSITION_DESTINATION_FILE_LOCATION];
 
-    InetSocketAddress address = new InetSocketAddress(hostName, portNumber);
-    System.out.println(address);
-
-    
-
-    InputStream sourceFileStream = new BufferedInputStream(new FileInputStream(sourceFileLocation));
-
-    
-
-    long sequenceNumber = 0;
-
-    while(destinationFileLocation.length() < 256) {
-      destinationFileLocation += " ";
-    }
-
-    CRC32 crc = new CRC32();
-      DatagramSocket sk = new DatagramSocket();
-      DatagramPacket pkt;
-
-    byte[] data = new byte[BLOCK_SIZE];
-    ByteBuffer b = ByteBuffer.wrap(data);
-
-    while(true) {
-      
-      int length;
-      if (sequenceNumber == 0) {
-        length = sourceFileStream.read(data, 0, CONTENT_WITH_NAME_SIZE);
-      }
-      else {
-        length = sourceFileStream.read(data, 0, CONTENT_SIZE);
-      }
-      if (length == -1) break;
-
-      int totalLength = length;
-
-      if (sequenceNumber == 0) {
-        totalLength += STRING_SIZE;
-      }
-
-      totalLength += CHECKSUM_SIZE + HEADER_SIZE;
-
-      ByteBuffer dataBuffer = ByteBuffer.allocate(totalLength);
-
-      // reserve space for checksum
-      dataBuffer.putLong(0);
-      System.out.println(sequenceNumber);
-      dataBuffer.putLong(sequenceNumber);
-
-      // add destination file name
-      if (sequenceNumber == 0) {
-        dataBuffer.put(destinationFileLocation.getBytes());
-      }
-    
-      dataBuffer.put(data, 0, length);
-
-      crc.reset();
-      crc.update(dataBuffer.array(), CHECKSUM_SIZE, totalLength-CHECKSUM_SIZE);
-      long chksum = crc.getValue();
-      dataBuffer.rewind();
-      dataBuffer.putLong(chksum);
-      System.out.println(totalLength);
-      pkt = new DatagramPacket(dataBuffer.array(), totalLength, address);
-      sk.send(pkt);
-
-      sequenceNumber++;
-    }
+    FileSender fileSender = new FileSender(hostName, portNumber);
+    fileSender.sendFile(sourceFileLocation, destinationFileLocation);
 
   }
 }
